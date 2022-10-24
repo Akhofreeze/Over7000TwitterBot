0f533599c23d59456368fad11dc33d0b47bba1a6
Akhoriz <victor.roz@orange.fr>
2022-10-24 22:10:13 +0200

    add python script

--- index.js
+++ index.js
@@ -2,16 +2,25 @@ const { Autohook } = require('twitter-autohook');
 
 (async start => {
   try {
-    const webhook = new Autohook();
+    const webhook = new Autohook({
+      token: '3elq1xfLI1qpuAJRcyljsRR97',
+      token_secret: 'JNymINTMHxEDDo9TOBM0EqDF4JuxVekiVRGNHGpiSohJ54LMSe',
+      consumer_key: 'RmRDS21tbGU1SDBzdkRfTUpIQWg6MTpjaQ',
+      consumer_secret: 'a44DJL6fHdWWeUY6fx8c5mrcbIlo3v2X0V8koxmjw9RVQNCHtq',
+      env: 'dev'
+    });
     
     // Removes existing webhooks
-    await webhook.removeWebhooks();
+    // await webhook.removeWebhooks();
     
     // Starts a server and adds a new webhook
-    await webhook.start();
+    await webhook.startServer();
+    webhook.on("event", event => console.log("Something happened:", event));
+    // await webhook.subscribe();
     
     // Subscribes to your own user's activity
-    await webhook.subscribe({oauth_token: process.env.TWITTER_ACCESS_TOKEN, oauth_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET});  
+    // // await webhook.subscribe({oauth_token: process.env.TWITTER_ACCESS_TOKEN, oauth_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET});
+    // await webhook.subscribe({oauth_token: "RmRDS21tbGU1SDBzdkRfTUpIQWg6MTpjaQ", oauth_token_secret: "a44DJL6fHdWWeUY6fx8c5mrcbIlo3v2X0V8koxmjw9RVQNCHtq" })  
   } catch (e) {
     // Display the error and quit
     console.error(e);
